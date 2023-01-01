93c87a70826d2014e1c8e7957ef0563f5bb4a28f
Hamza Laabidi <99836375+hamzaabidi28@users.noreply.github.com>
2023-01-01 23:19:52 +0100

    fixing navToggle

---
+++ main.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute('data-visible') ?
+        navToggle.setAttribute("aria-expanded", false) :
+        navToggle.setAttribute("aria-expanded", true);
+    primaryNav.toggleAttribute("data-visible");
+});
